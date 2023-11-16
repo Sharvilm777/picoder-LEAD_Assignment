f433e4f7cf61d9a40ae2c60a2e0d4aacab28a9e9
Sharvil <sharvilm777@gmail.com>
2023-11-16 23:39:06 +0530

    Bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -136,6 +136,7 @@ function App() {
             <div className="flex justify-center items-center">
               <button
                 onClick={() => {
+                  setenteredPincode("");
                   dispatch(clearDetail());
                 }}
                 className="inline-flex items-center py-2.5 px-3 m-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
